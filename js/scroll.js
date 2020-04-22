61f45a786d61fe5bd456a9adb3518c90c1d47817
dosu416 <dosu416@gmail.com>
2020-04-22 14:34:19 +0900

    [ADD] スクロールしたらフェードインする

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,25 @@
 $(function() {
-  $('a[href^=#]').click(function() {
-    var speed = 400;
-    var href= $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    $('body,html').animate({scrollTop:position}, speed, 'swing');
+  $('li[href^="#"]').click(function() {
+    const duration = 750;
+    const headerOuterHeight = $('header').outerHeight(true);  // included margin 
+    const href = $(this).attr("href");
+    const target = $(href == "#" || href == "" ? 'html' : href);
+    const position = target.offset().top - headerOuterHeight;
+    $('body,html').animate({scrollTop:position}, duration, 'swing');
     return false;
+  });
+});
+
+$(function() {
+  $('li[id^="costume"]').click(function() {
+    imgId = $(this).attr("id");
+    imgPath = 'img/' + imgId + '.jpg';
+    $('img#mainchara').attr('src', imgPath);
+  });
+});
+
+$(function() {
+  $('.fadein').on('inview', function() {
+    $(this).children().addClass('animation');
   });
 });
